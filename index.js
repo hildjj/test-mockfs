a88c18c023b86a26761f6e6cfc1836ff6e078ace
Joe Hildebrand <joe-github@cursive.net>
2024-02-10 20:01:06 -0700

    add others

--- index.js
+++ index.js
@@ -2,12 +2,17 @@
 
 const mockFs = require('mock-fs');
 const fs = require('fs');
+const path = require('path');
+
 const cwd = process.cwd();
 console.log(cwd);
+const posix_cwd = cwd.split(path.sep).join(path.posix.sep);
+const other = posix_cwd.replace(/^[A-Z]:/, '');
 
 mockFs({
   '/b': 'foo',
-
+  [`${posix_cwd}/bar`]: 'bar',
+  [`${other}/goo`]: 'bag',
 });
 console.log(fs.readdirSync('/'))
 console.log(fs.readdirSync(cwd))
