ee0e477d968ae7a44ecf4207a0a4c6ffa777c865
Joe Hildebrand <joe-github@cursive.net>
2024-02-10 20:06:30 -0700

    load

--- index.js
+++ index.js
@@ -5,15 +5,17 @@ const fs = require('fs');
 const path = require('path');
 
 const cwd = process.cwd();
-console.log(cwd);
 const posix_cwd = cwd.split(path.sep).join(path.posix.sep);
-const other = posix_cwd.replace(/^[A-Z]:/, '');
+// const other = posix_cwd.replace(/^[A-Z]:/, '');
+console.log(cwd, posix_cwd);
 
 mockFs({
-  '/b': 'foo',
-  [`${posix_cwd}/bar`]: 'bar',
-  [`${other}/goo`]: 'bag',
+  [`${posix_cwd}/node_modules`]: mockFs.load(path.join(cwd, node_modules)),
+}, {
+  createCwd: false,
+  createTmp: false,
 });
+
 console.log(fs.readdirSync('/'))
 console.log(fs.readdirSync(cwd))
 mockFs.restore();
