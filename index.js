5854a22e013a0808cdd26e5373a3614d3cf9364c
Joe Hildebrand <joe-github@cursive.net>
2024-02-10 19:57:00 -0700

    cwd

--- index.js
+++ index.js
@@ -2,8 +2,13 @@
 
 const mockFs = require('mock-fs');
 const fs = require('fs');
+const cwd = process.cwd();
+console.log(cwd);
+
 mockFs({
   '/b': 'foo',
+
 });
 console.log(fs.readdirSync('/'))
+console.log(fs.readdirSync(cwd))
 mockFs.restore();
